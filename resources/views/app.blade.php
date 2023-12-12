91918f8bd58933f15a318b983eecbbd2d4cff3e1
Sulaiman Desktop <msulaimanmisri@gmail.com>
2023-12-12 22:22:20 +0800

    Install Inertia for ServerSide

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laranertia</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
