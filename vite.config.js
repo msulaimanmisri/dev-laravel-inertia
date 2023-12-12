3e1c19ceed32eddbec13b0a27bf2c8386fc2c868
Sulaiman Desktop <msulaimanmisri@gmail.com>
2023-12-12 12:19:23 +0800

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import { version } from "vue";
 
 export default defineConfig({
     plugins: [
@@ -8,13 +9,6 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(3),
     ],
 });
